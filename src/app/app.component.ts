c80758c7610bdd91a48b489da95cbd4c476225f4
Michael Waweru <68115161+michael-waweru@users.noreply.github.com>
2025-02-21 13:22:16 +0300

    header components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponentComponent } from "./partials/header-component/header-component.component";
+import { FooterComponentComponent } from "./partials/footer-component/footer-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponentComponent, FooterComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
