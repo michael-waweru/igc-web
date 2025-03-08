ca21fd7b21cad4933fa6693ec0dd89708f30345a
Michael Waweru <68115161+michael-waweru@users.noreply.github.com>
2025-03-08 18:40:12 +0300

    frontend updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AboutComponentComponent } from './pages/about-component/about-component
 import { HomeComponentComponent } from './pages/home-component/home-component.component';
 import { ModulesComponentComponent } from './pages/modules-component/modules-component.component';
 import { ContactComponentComponent } from './pages/contact-component/contact-component.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 export const routes: Routes = [
   { path: '', component:HomeComponentComponent },
   { path: 'about-us', component:AboutComponentComponent},
   { path: 'modules', component: ModulesComponentComponent},
+  { path: 'blog', component: BlogComponent},
   { path: 'contact-us', component: ContactComponentComponent},
 ];
 
