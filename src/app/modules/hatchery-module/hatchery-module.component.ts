3c5c39af14fcbc6eb0626793fca655f5c0c513f0
Michael Waweru <68115161+michael-waweru@users.noreply.github.com>
2025-03-20 20:31:04 +0300

    map-changes

--- src/app/modules/hatchery-module/hatchery-module.component.ts
+++ src/app/modules/hatchery-module/hatchery-module.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hatchery-module',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hatchery-module.component.html',
   styleUrl: './hatchery-module.component.css'
 })
