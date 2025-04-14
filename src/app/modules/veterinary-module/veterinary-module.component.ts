e57774f10d99a9ebc5aceb5e76cafab46ed95744
Michael Waweru <68115161+michael-waweru@users.noreply.github.com>
2025-04-14 22:02:06 +0300

    urls

--- src/app/modules/veterinary-module/veterinary-module.component.ts
+++ src/app/modules/veterinary-module/veterinary-module.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-veterinary-module',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './veterinary-module.component.html',
   styleUrl: './veterinary-module.component.css'
 })
