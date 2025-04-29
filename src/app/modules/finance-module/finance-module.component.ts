34f98c27060a6974e7cf45d480197418b0d7244d
Michael Waweru <68115161+michael-waweru@users.noreply.github.com>
2025-04-29 21:58:40 +0300

    f.m

--- src/app/modules/finance-module/finance-module.component.ts
+++ src/app/modules/finance-module/finance-module.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-finance-module',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './finance-module.component.html',
   styleUrl: './finance-module.component.css'
 })
